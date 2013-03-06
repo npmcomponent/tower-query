a45e0cb293fde5578e420f1d85722c680502a56d
Lance Pollard <lancejpollard@gmail.com>
2013-03-05 18:03:59 -0800

    action and docs

--- index.js
+++ index.js
@@ -38,13 +38,39 @@ pipeline.where = function(key, val){
   return this.condition('eq', key, val);
 }
 
+/**
+ * In a graph database, the data pointing _to_ this node.
+ * In a relational/document database, the records with
+ * a foreign key pointing to this record or set of records.
+ *
+ * @param {String} key
+ * @api public
+ */
+
 pipeline.incoming = function(key){
   return this.relation('incoming', key);
 }
 
+/**
+ * In a graph database, the data pointing _from_ this node.
+ * In a relational/document database, the record this
+ * record points to via its foreign key.
+ *
+ * @param {String} key
+ * @api public
+ */
+
 pipeline.outgoing = function(key){
   return this.relation('outgoing', key);
 }
+
+/**
+ * What the variable should be called for the data returned.
+ * References the previous item in the pipeline.
+ *
+ * @param {String} key
+ * @api public
+ */
 
 pipeline.as = function(key){
   return this.push('as', key);
@@ -66,7 +92,23 @@ pipeline.lt = function(key, val){
   return this.condition('lt', key, val);
 }
 
+pipeline.insert = function(data){
+  return this.action('insert', data);
+}
+
+pipeline.update = function(data){
+  return this.action('update', data);
+}
+
+pipeline.remove = function(data){
+  return this.action('remove', data);
+}
+
 /**
+ * Pushes a `"relation"` into the pipeline.
+ *
+ * @param {String} type
+ * @param {String} key
  * @api private
  */
 
@@ -75,13 +117,38 @@ pipeline.relation = function(type, key){
 }
 
 /**
+ * Pushes a `"condition"` into the pipeline.
+ *
  * @param {String} op Operator string
+ * @param {String} key
+ * @param {Object} val
  * @api private
  */
 
 pipeline.condition = function(op, key, val){
   return this.push('condition', op, key, val);
 }
+
+/**
+ * Pushes an `"action"` into the pipeline.
+ *
+ * Example:
+ *
+ *    pipeline().action('insert', {message: 'Test'});
+ *    pipeline().action('insert', [{message: 'one.'}, {message: 'two.'}]);
+ *
+ * @param {String} type
+ * @param {Object|Array} data The data to act on.
+ * @api private
+ */
+
+pipeline.action = function(type, data){
+  return this.push('action', type, data);
+}
+
+/**
+ * 
+ */
 
 /**
  * @api private
